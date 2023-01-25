a16ab5b42c8a951b3d89fdcb7ff22415174d6afd
thisiscyiran <farzad.dianat@gmail.com>
2023-01-25 19:55:40 +0330

    "Change the name of second parameter times"

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,11 +1,12 @@
-function repeatString(string, num = 3) {
+function repeatString(string, times = 3) {
   let newString = "";
 
-  if (Math.sign(num) === -1) {
+  if (Math.sign(times) === -1) {
+    // if times is negative
     return "ERROR";
   }
 
-  for (let i = 1; i <= num; i++) {
+  for (let i = 1; i <= times; i++) {
     newString += string;
   }
   return newString;
