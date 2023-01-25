fda205e85c2631e31c10502870c0d7ca9866e870
thisiscyiran <farzad.dianat@gmail.com>
2023-01-25 19:07:42 +0330

    "Add Solutions to exercise 01 through 07"

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (f) {
+  // formula: C = (f - 32) / 1.8
+  let c = (f - 32) / 1.8;
+  let cRounded = Math.round(c * 10) / 10; // rounded c to 1 decimal place
+  return cRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (c) {
+  // formula: C * 1.8 + 32 = F
+  let f = c * 1.8 + 32;
+  let fRounded = Math.round(f * 10) / 10; // Rounded f to 1 decimal place
+  return fRounded;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
